7a2b29989cd6d0bd81dae1bf7f0b04edec703239
Andrew Lim <Andrew.Lim@e14.io>
2025-05-22 16:02:41 -0300

    Finished button exercise

--- examples/element-factory/src/button.test.js
+++ examples/element-factory/src/button.test.js
@@ -1,9 +1,26 @@
+import { screen, fireEvent } from '@testing-library/dom'
+import userEvent from '@testing-library/user-event'
 import { createButton } from './button.js';
 
-describe.todo('createButton', () => {
-  it('should create a button element', () => {});
+describe('createButton', () => {
+  afterEach(() => {
+    document.body.innerHTML = '';
+  })
+  it('should create a button element', () => {
+    // const button = createButton()
+    // expect(button).toBeInstanceOf(HTMLButtonElement)
 
-  it('should have the text "Click Me"', () => {});
+    document.body.appendChild(createButton());
+    const button = screen.getByRole('button', { name: 'Click Me'})
+    expect(button).toBeInTheDocument()
+  });
 
-  it('should change the text to "Clicked!" when clicked', async () => {});
+  it('should change the text to "Clicked!" when clicked', async () => {
+    document.body.appendChild(createButton())
+
+    const button = screen.getByRole('button', { name: 'Click Me'})
+    await userEvent.click(button)
+    
+    expect(button.textContent).toBe('Clicked!')
+  });
 });
