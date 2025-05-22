1204291675ea33355342bb9bef7db3ac8557236d
Andrew Lim <Andrew.Lim@e14.io>
2025-05-22 17:03:12 -0300

    Finished video one of accident counter exercise

--- examples/accident-counter/src/counter.test.jsx
+++ examples/accident-counter/src/counter.test.jsx
@@ -5,40 +5,34 @@ import { Counter } from './counter';
 
 import '@testing-library/jest-dom/vitest';
 
-describe.todo('Counter ', () => {
+describe('Counter', () => {
   beforeEach(() => {
     render(<Counter />);
   });
 
-  it('renders with an initial count of 0');
+  it('renders with an initial count of 0', () => {
+    const counter = screen.getByTestId('counter-count');
+    expect(counter).toHaveTextContent('0');
+  });
 
-  it('disables the "Decrement" and "Reset" buttons when the count is 0');
+  it('disables the "Decrement" and "Reset" buttons when the count is 0', () => {
+    const decrementButton = screen.getByRole('button', { name: /decrement/i });
+    const resetButton = screen.getByRole('button', { name: /reset/i });
+    expect(decrementButton).toBeDisabled();
+    expect(resetButton).toBeDisabled();
+  });
 
-  it.todo('displays "days" when the count is 0', () => {});
+  it.todo('displays "days" when the count is 0');
 
-  it.todo(
-    'increments the count when the "Increment" button is clicked',
-    async () => {},
-  );
+  it.todo('increments the count when the "Increment" button is clicked');
 
-  it.todo('displays "day" when the count is 1', async () => {});
+  it.todo('displays "day" when the count is 1');
 
-  it.todo(
-    'decrements the count when the "Decrement" button is clicked',
-    async () => {},
-  );
+  it.todo('decrements the count when the "Decrement" button is clicked');
 
-  it.todo('does not allow decrementing below 0', async () => {});
+  it.todo('does not allow decrementing below 0');
 
-  it.todo(
-    'resets the count when the "Reset" button is clicked',
-    async () => {},
-  );
+  it.todo('resets the count when the "Reset" button is clicked');
 
-  it.todo(
-    'disables the "Decrement" and "Reset" buttons when the count is 0',
-    () => {},
-  );
-
-  it.todo('updates the document title based on the count', async () => {});
+  it.todo('updates the document title based on the count');
 });
